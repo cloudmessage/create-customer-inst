d0dec97e2603d99578fe24906ad9893945a4ebd9
Kalyan Vedala <svedala23@gmail.com>
2023-08-03 21:08:37 -0700

    Issue http calls to create virtual host and user

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib/callback_api');
+const axios = require('axios')
 const randomstring = require('randomstring')
 
 function getRandomUsernameAndVhost() {
@@ -44,9 +45,46 @@ amqp.connect('amqp://localhost:10001', function(error0, connection) {
 
       const randomString = getRandomUsernameAndVhost()
       const password = generatePassword()
-      console.log("random-string=", randomString, "password=", password)
-      // TODO: create vhost and user
+
       console.log("Creating vhost and user")
+
+      const managementCredentials = "guest:guest"
+      const base64ManagementCredentials = Buffer.from(managementCredentials).toString("base64")
+      const config = {
+        headers: {
+          "content-type": "application/json",
+          "Authorization": "Basic " + base64ManagementCredentials
+        }
+      }
+
+      // creating vhost
+      axios.put(
+        'http://localhost:15672/api/vhosts/' + randomString,
+        null,
+        config
+      )
+      .then(() => {
+        console.log(`host ${randomString} created`)
+      })
+      .catch((err) => {
+        console.log("host create error: ", err)
+        throw err
+      })
+
+      // creating user
+      axios.put(
+        'http://localhost:15672/api/users/' + randomString,
+        {"password": password, "tags": "customer"},
+        config
+      )
+      .then(() => {
+        console.log(`user ${randomString} created`)
+      })
+      .catch((err) => {
+        console.log("user create error: ", err)
+        throw err
+      })
+
       channel.ack(msg);
     }, {
       // manual acknowledgment mode,
