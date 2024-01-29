585d0e46f2bb0284c1f363f744096b861328ee04
Kalyan Vedala <svedala23@gmail.com>
2024-01-29 05:41:42 -0800

    Fix indentation

--- createCustomer.js
+++ createCustomer.js
@@ -4,26 +4,26 @@ import { getRandomUsernameAndVhost, generatePassword } from './utils.js';
 
 const createCustomerVhostAndUser = function(channel, msg) {
   (async () => {
-  var instanceId = msg.content.toString();
+    var instanceId = msg.content.toString();
 
-  console.log(" [x] Received %s", instanceId);
+    console.log(" [x] Received %s", instanceId);
 
-  const randomString = getRandomUsernameAndVhost()
-  const password = generatePassword()
+    const randomString = getRandomUsernameAndVhost()
+    const password = generatePassword()
 
-  console.log("Creating vhost and user")
+    console.log("Creating vhost and user")
 
-  await custClusterApi.createVhost(randomString);
-  await custClusterApi.createUser(randomString, password);
-  await custClusterApi.grantPermissions(randomString);
+    await custClusterApi.createVhost(randomString);
+    await custClusterApi.createUser(randomString, password);
+    await custClusterApi.grantPermissions(randomString);
 
-  const url = new URL(process.env.CUSTOMER_CLUSTER_URL);
-  const hostname = url.hostname;
+    const url = new URL(process.env.CUSTOMER_CLUSTER_URL);
+    const hostname = url.hostname;
 
-  // update database with instance information
-  Data.updateDatabase(instanceId, randomString, password, hostname);
+    // update database with instance information
+    Data.updateDatabase(instanceId, randomString, password, hostname);
 
-  channel.ack(msg);
+    channel.ack(msg);
   })();
 
 };
