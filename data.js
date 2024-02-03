1fc3cf452e74eb9db7669b13b5ce0a20da58727e
Kalyan Vedala <svedala23@gmail.com>
2024-02-03 08:10:41 -0800

    Inject knex to data.updateDatabase

--- data.js
+++ data.js
@@ -1,10 +1,5 @@
-import { getKnexEnvOptions } from './knexoptions.js';
-import Knex from 'knex';
 
-const updateDatabase = (instanceId, userAndVirtualHost, password, hostname) => {
-  const knexOptions = getKnexEnvOptions(process.env.NODE_ENV, process.env.FILENAME_OR_DB_URL);
-  const knex = Knex(knexOptions);
-
+const updateDatabase = (knex, instanceId, userAndVirtualHost, password, hostname) => {
   knex('instances')
   .where('id', instanceId)
   .update({
@@ -16,4 +11,4 @@ const updateDatabase = (instanceId, userAndVirtualHost, password, hostname) => {
   .catch((err) => { console.log(err); throw err });
 }
 
-export default { updateDatabase };
+export { updateDatabase };
