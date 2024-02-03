74facee7c6a887dbb7aa0675209b2d89846283a0
Kalyan Vedala <svedala23@gmail.com>
2024-02-03 10:18:26 -0800

    Convert data to class - for dependency inversion

--- data.js
+++ data.js
@@ -1,14 +1,20 @@
 
-const updateDatabase = (knex, instanceId, userAndVirtualHost, password, hostname) => {
-  knex('instances')
-  .where('id', instanceId)
-  .update({
-    user: userAndVirtualHost,
-    virtual_host: userAndVirtualHost,
-    password,
-    hostname
-  })
-  .catch((err) => { console.log(err); throw err });
+class Data {
+  constructor(knex) {
+    this.knex = knex;
+  }
+
+  updateDatabase(instanceId, userAndVirtualHost, password, hostname) {
+    this.knex('instances')
+    .where('id', instanceId)
+    .update({
+      user: userAndVirtualHost,
+      virtual_host: userAndVirtualHost,
+      password,
+      hostname
+    })
+    .catch((err) => { console.log(err); throw err });
+  }
 }
 
-export { updateDatabase };
+export default Data;
